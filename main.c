579eb3809b35027ec6beb7d7bdaea328f2f988f6
Iarina1 <49359405+Iarina1@users.noreply.github.com>
2023-04-15 15:44:01 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World2!");
+   printf("Hello, World doamne ajuta sa mearga!");
    return 0;
 }
