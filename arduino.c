c4c7cef14ee13091d00e90245b378bc0caa6c84b
Iarina1 <49359405+Iarina1@users.noreply.github.com>
2023-04-20 15:26:53 +0300

    Create arduino.c

---
+++ arduino.c
@@ -0,0 +1,7 @@
+void setup() {
+  Serial.begin(9600); // Initialize serial communication at 9600 bits per second
+  Serial.println("Hello, World!"); // Send "Hello, World!" over the serial connection
+}
+
+void loop() {
+}
