8f9b53703f769bafde5953262f7e044c69ba6720
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-05-13 15:22:01 +0300

    Update problema1.ino

--- problema1.ino
+++ problema1.ino
@@ -1,67 +1,19 @@
-unsigned long t_debounce_1, t_debounce_2 = 0;
-int dt = 500;
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
 
-ISR(INT0_vect)
-{
-  // cod întrerupere externă
-  unsigned long t_int = millis();
-  if (t_int - t_debounce_1 > 300) {    
-    t_debounce_1 = t_int;
-    dt += 100;
-  }
-}
-
-ISR(PCINT2_vect) {
-  // cod întrerupere de tip pin change
-  if ((PIND & (1 << PD4)) == 0) {
-    // întrerupere generată de pinul PD4 / PCINT20
-
-    unsigned long t_int = millis();
-    if (t_int - t_debounce_2 > 300) {
-      t_debounce_2 = t_int;
-      if (dt > 100) {
-         dt -= 100;
-      }      
-    }
-  }
-}
-
-void setup_interrupts() {
-  // buton 1: PD2 / INT0
-  // buton 2: PD4 / PCINT20
-  cli();
-
-  // input
-  DDRD &= ~(1 << PD2) & ~(1 << PD4);
-  // input pullup
-  PORTD |= (1 << PD2) | (1 << PD4);
-
-  // configurare intreruperi
-  // intreruperi externe
-  EICRA |= (1 << ISC01);    // set INT0 to trigger on falling edge (1-0)
-  // întreruperi de tip pin change (activare vector de întreruperi)
-  PCICR |= (1 << PCIE2); // enable the pin change interrupt, set PCIE2 to enable PCMSK2 scan
-
-  // activare intreruperi
-  // intrerupere externa
-  EIMSK |= (1 << INT0);     // Turns on INT0
-  // întrerupere de tip pin change
-  PCMSK2 |= (1 << PCINT20); // Turns on PCINT20 (PD4)
-
-  sei();
-}
-
+  This example code is in the public domain.
+ */
 
 void setup() {
-  setup_interrupts();
-  // test led
-  DDRD |= (1 << PD7);
-  PORTD &= ~(1 << PD7);
+  // initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards:
+  pinMode(13, OUTPUT);
 }
 
-void loop() {  
-  PORTD |= (1 << PD7);
-  delay(dt);
-  PORTD &= ~(1 << PD7);
-  delay(dt);
+void loop() {
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(1000);              // wait for a second
 }
