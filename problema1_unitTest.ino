4f8c2fbd22e623a85cf9c57e5d68c1a43241ae5b
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-06-03 22:21:30 +0300

    Update problema1_unitTest.ino

--- problema1_unitTest.ino
+++ problema1_unitTest.ino
@@ -6,11 +6,16 @@ uint8_t led_pin = 5;
 void test_led_state_high(void) {
     turn_led_on(led_pin);
     TEST_ASSERT_EQUAL(LOW, digitalRead(led_pin));
+    if (led_state == HIGH) {
+        Serial.println("LED state is HIGH. Test passed!");
+    } else {
+        Serial.println("LED state is LOW. Test failed!");
+    }
 }
 
 void setup() {
     delay(2000);
-
+    Serial.begin(9600);
     UNITY_BEGIN();
     RUN_TEST(test_led_state_high);
     UNITY_END();
