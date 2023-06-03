022aab34e22ec6302b97657cf51cb1af1c27ec87
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-06-03 18:29:25 +0300

    Update run_tests.sh

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -1,1 +1,1 @@
-arduino-cli compile --output tempTest.bin -b esp32:esp32:esp32 $PWD/.circleci/problema1_test.ino --debug
+arduino-cli compile --output tempTest.bin -b esp32:esp32:esp32 .circleci/problema1_test.ino --debug
