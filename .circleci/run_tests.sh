7698efc7fd734d9bc042be75a4bb75ecd0516771
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-06-03 23:05:24 +0300

    Update run_tests.sh

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -1,3 +1,3 @@
 mkdir $PWD/test-results
-arduino-cli compile -b esp32:esp32:esp32 --output-name output $PWD/problema1_unitTest.ino
+arduino-cli compile -b esp32:esp32:esp32 -o output $PWD/problema1_unitTest.ino
 arduino-cli upload -p auto -b esp32:esp32:esp32 $PWD/output.hex | tee /tmp/test_output.txt 
