00f91a3cde1e3645604afe019870554810517846
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-06-03 22:22:51 +0300

    Update problema1_unitTest.ino

--- problema1_unitTest.ino
+++ problema1_unitTest.ino
@@ -5,6 +5,7 @@ uint8_t led_pin = 5;
 
 void test_led_state_high(void) {
     turn_led_on(led_pin);
+    int led_state = digitalRead(led_pin);
     TEST_ASSERT_EQUAL(LOW, digitalRead(led_pin));
     if (led_state == HIGH) {
         Serial.println("LED state is HIGH. Test passed!");
