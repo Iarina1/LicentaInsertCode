e7b8ef115f0b41d243497b3bad3e26505a50e27b
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-06-03 19:14:02 +0300

    Update run_tests.sh

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -1,2 +1,2 @@
 mkdir $PWD/test-results
-arduino-cli compile --build-path $PWD/test-results --output tempTest.bin -b esp32:esp32:esp32 $PWD/problema1_unitTest.ino --debug
+arduino-cli compile --output tempTest.bin -b esp32:esp32:esp32 $PWD/problema1_unitTest.ino --show-properties --format json -v
