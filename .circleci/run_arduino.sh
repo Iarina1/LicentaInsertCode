04ee0b5a9e634a7251d4e81df1f3ad27c36d41f2
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-06-03 18:24:17 +0300

    Update run_arduino.sh

--- .circleci/run_arduino.sh
+++ .circleci/run_arduino.sh
@@ -1,4 +1,2 @@
 # git clone https://github.com/ThrowTheSwitch/Unity.git
 arduino-cli compile --output temp.bin -b esp32:esp32:esp32 $PWD/problema1.ino --debug
-cat $PWD/.circleci/problema1_test.ino
-arduino-cli compile --output temp.bin -b esp32:esp32:esp32 $PWD/.circleci/problema1_test.ino --debug
